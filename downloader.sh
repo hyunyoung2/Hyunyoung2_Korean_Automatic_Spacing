183af2f445a8595625c3e478f730729ba5b1ecfb
hyunyoung2 <hyun02.engineer@gmail.com>
2018-08-16 20:29:47 +0900

    final setting

--- downloader.sh
+++ downloader.sh
@@ -3,13 +3,14 @@
 # this shell script is downloader for model trained. 
 
 echo "downloading........"
+echo
 
 MODEL="model.tar.gz"
 
 curl -O "http://nlp.kookmin.ac.kr/hyunyoung2/autospacing/${MODEL}"
 
-
 echo "uncompressing......"
+echo 
 
 tar -xvzf ${MODEL}
 
